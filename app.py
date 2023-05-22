4ffa716024dd86ebd2cd24bdf25c1ea71a158981
iss9802 <iss9802@snu.ac.kr>
2023-05-22 15:52:41 +0900

    commit6

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from model import *
 from transformers import AutoTokenizer
+from transformers import BertTokenizer
+import streamlit as st
+import torch
+import numpy as np
 """
 자신의 모델에 맞는 변수 설정해주기
 """
@@ -10,6 +14,7 @@ emb = 16 #default 값 지정 안했으면 건드리지 않아도 됨
 hidden = 32 #default 값 지정 안했으면 건드리지 않아도 됨
 nh = 4 #default 값 지정 안했으면 건드리지 않아도 됨
 device = "cpu" #default 값 지정 안했으면 건드리지 않아도 됨
+max_len = 100
 #output_d 설정
 output_d = 5 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
 c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
@@ -18,7 +23,53 @@ c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
 """
 model과 tokneizer 부르기
 """
-model = RNNModel(output_d, c)
+model = RNNModel(output_d, c) #RNNModel 쓰는경우
+# model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
+# model = ATTModel(output_d, c) #ATTModel 쓰는경우
+
 model.load_state_dict(torch.load("./save/"+model_name+".pt"))
+
 #자신에게 맞는 모델로 부르기
-tokenizer = AutoTokenizer.from_pretrained("./save/"+ model_name)
+tokenizer = AutoTokenizer.from_pretrained("./save/"+ model_name) #sp tokenizer 쓰는 경우
+# tokenizer = BertTokenizer.from_pretrained("./save/"+model_name+"-vocab.txt") #bw tokenizer 쓰는경우
+
+"""
+여기서부터는 웹에 들어갈 내용
+관련된 함수 참고 : https://docs.streamlit.io/
+"""
+
+st.set_page_config(
+  page_title = "2023_수교평_자동피드백",
+  layout = "wide",
+)
+
+st.title("자동 채점 모델 기반 자동 피드백")
+st.write("**팀원** : 수학교육과 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진")
+
+st.subheader("문항3-3")
+st.markdown("$A \div 3y/2 = 4x^{2}y + 2xy +6$ 일 때 다항식 $A$ 를 구하시오")
+response = st.text_input('답안 :', "답안을 작성해주세요")
+
+"""
+자동 채점해주는 코드
+"""
+enc = tokenizer(response)["input_ids"] #sp tokenizer
+# enc = tokenizer.encode(response) #bw tokenizer
+l = len(response)
+if l < 100 :
+    pad = (100 - l) * [0] + enc
+else : pad = enc[l-100:]
+pad_ten = torch.tensor(pad).reshape(1,max_len)
+y = model(pad_ten)
+label = y.squeeze().detach().cpu().numpy().round()
+
+if st.button('피드백 받기'):
+    """
+    output차원에 맞추어 피드백 넣기
+    """
+    if label[1] == 1:
+        st.success('(다항식) 곱하기 (단항식)을 잘하는구나!', icon="✅")
+    else :
+        st.info('(다항식) 곱하기 (단항식)을 잘 생각해보자!', icon="ℹ️")
+else : 
+    st.button('피드백 받기 버튼을 눌러보세요!')
