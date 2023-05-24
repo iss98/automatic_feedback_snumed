59e7f5fce150aea3e515b33003c86a8c62d88790
Insub Shin <iss9802@snu.ac.kr>
2023-05-24 19:57:44 +0900

    commit18

--- app.py
+++ app.py
@@ -5,21 +5,15 @@ import streamlit as st
 import torch
 import numpy as np
 
-"""
-여기서부터는 웹에 들어갈 내용
-관련된 함수 참고 : https://docs.streamlit.io/
-"""
-
 st.title("자동 채점 모델 기반 자동 피드백")
 st.write("**팀원** : 수학교육과 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진")
 
 st.subheader("문항3-3")
 st.markdown("$A \div 3y/2 = 4x^{2}y + 2xy +6$ 일 때 다항식 $A$ 를 구하시오")
+st.write("안녕! 나는 자동채점하는 기계야!")
 response = st.text_input('답안 :', "답안을 작성해주세요")
 
-"""
-자신의 모델에 맞는 변수 설정해주기
-"""
+#모델의 이름 정하기
 model_name = "3-3_rnn_sp_100" #모델 이름 넣어주기 확장자는 넣지말기!
 #모델에 맞는 hyperparameter 설정
 vs = 100 #vocab size
@@ -32,10 +26,6 @@ max_len = 100
 output_d = 5 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
 c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
 
-
-"""
-model과 tokneizer 부르기
-"""
 model = RNNModel(output_d, c) #RNNModel 쓰는경우
 # model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
 # model = ATTModel(output_d, c) #ATTModel 쓰는경우
@@ -46,15 +36,12 @@ model.load_state_dict(torch.load("./save/"+model_name+".pt"))
 tokenizer = AutoTokenizer.from_pretrained("./save/"+ model_name) #sp tokenizer 쓰는 경우
 # tokenizer = BertTokenizer.from_pretrained("./save/"+model_name+"-vocab.txt") #bw tokenizer 쓰는경우
 
-"""
-자동 채점해주는 코드
-"""
 enc = tokenizer(response)["input_ids"] #sp tokenizer
 # enc = tokenizer.encode(response) #bw tokenizer
 l = len(enc)
-if l < 100 :
-    pad = (100 - l) * [0] + enc
-else : pad = enc[l-100:]
+if l < max_len :
+    pad = (max_len - l) * [0] + enc
+else : pad = enc[l-max_len:]
 pad_ten = torch.tensor(pad)
 pad_ten = pad_ten.reshape(1,max_len)
 y = model(pad_ten)
