66373b34f1ba5b7e2be6959c1c6751bcbbe3e819
iss9802 <iss9802@snu.ac.kr>
2023-06-01 16:29:31 +0900

    edit28

--- pages/7_결과.py
+++ pages/7_결과.py
@@ -44,7 +44,7 @@ with col6:
     st.image('images/2_1_2_6_testacc.png', use_column_width=True, caption = "Test Accuracy")
 markdown_table2 = """
 |  | **2-1** | **2-2** | **2-3** | **2-4** | **2-5** | **2-6** | **2-7** | **2-8** | **2-9**|
-| --- | --- | --- | --- | --- | --- | --- | --- | --- | |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
 | RNN | |  | | | | | | | |
 | LSTM | |  | | | | | | | |
 | Attention | | | | | | | | | |
@@ -71,10 +71,47 @@ st.markdown(markdown_table3, unsafe_allow_html=True)
 st.write("결과 정리")    
 st.divider()
 st.header("2. 오개념 모델")
-st.subheader("파트1")
-st.subheader("파트2")
-st.subheader("파트3")
-st.divider()
+
+col10, col11, col12 = st.columns(3)
+with col10 :
+    st.image('images/2_1_acc.png', use_column_width=True, caption = "Accuracy")
+    st.image('images/2_2_acc.png', use_column_width=True, caption = "Accuracy")
+    st.image('images/2_3_acc.png', use_column_width=True, caption = "Accuracy")
+with col11:
+    st.image('images/2_1_loss.png', use_column_width=True, caption = "loss")
+    st.image('images/2_2_loss.png', use_column_width=True, caption = "loss")
+    st.image('images/2_3_loss.png', use_column_width=True, caption = "loss")
+with col12:
+    st.image('images/2_1_testacc.png', use_column_width=True, caption = "Test Accuracy")
+    st.image('images/2_2_testacc.png', use_column_width=True, caption = "Test Accuracy")
+    st.image('images/2_3_testacc.png', use_column_width=True, caption = "Test Accuracy")
+
+markdown_table4 = """
+|  | **1-1** | **1-2** | **1-3** | **1-4** | **1-5** | **1-6** | **1-7** | **1-8** |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| RNN | |  | | | | | | |
+| LSTM | |  | | | | | | |
+| Attention | | | | | | | | |
+"""
+st.markdown(markdown_table4, unsafe_allow_html=True)
+markdown_table5 = """
+|  | **2-1** | **2-2** | **2-3** | **2-4** | **2-5** | **2-6** | **2-7** | **2-8** | **2-9**|
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| RNN | |  | | | | | | | |
+| LSTM | |  | | | | | | | |
+| Attention | | | | | | | | | |
+"""
+st.markdown(markdown_table5, unsafe_allow_html=True)
+markdown_table6 = """
+|  | **3-1** | **3-2** | **3-3** |
+| --- | --- | --- | --- |
+| RNN | |  | |
+| LSTM | |  | |
+| Attention | | | |
+"""
+st.markdown(markdown_table6, unsafe_allow_html=True)
+st.write("결과 정리")
+
 st.header("3. 정오답 채점 모델")
 st.subheader("파트1")
 st.subheader("파트2")
