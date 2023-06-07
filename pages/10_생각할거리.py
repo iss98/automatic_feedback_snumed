5cfab176b5fb6bb2297c3dc83b720e7bf5b663d2
Insub Shin <iss9802@snu.ac.kr>
2023-06-07 10:12:38 +0900

    Update 10

--- pages/10_생각할거리.py
+++ pages/10_생각할거리.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 st.title(":thinking_face: 생각할거리")
-st.write("피드백을 만들던 중 당연한 오답을 모델이 정확히 처리하지 못하는 것을 확인할 수 있었음")
+st.divider()
+st.write(":one: 피드백을 만들던 중 당연한 오답을 모델이 정확히 처리하지 못하는 것을 확인할 수 있었음")
 st.write(":point_right: 학습의 과정에서 학생들이 보여줬던 반응들만 분석 가능하다는 모델의 한계를 확인 가능함")
 st.write(":point_right: 일반화의 관점에서 모델의 성능을 높일 방법을 고민할 필요가 있음")
+st.divider()
+st.write(":two: 학생의 학습과정을 반영한 피드백을 생성하기 어려움")
+st.write(":point_right: 인공지능은 문항에 대한 답변만 가지고 피드백을 생성함")
+st.write(":point_right: 같은 공란이더라도 학생이 답안을 작성하지 않은 이유가 다를 것")
+st.write(":point_right: 교사의 판단과 분석이 중요할 것으로 보임")
+st.divider()
+st.write(":three: 실제 교육현장에서의 적용을 고려한다면 cold start problem을 고민해야할 것으로 보임")
+st.write(":point_right: 거대 언어 모델 사용?"
+st.divider()
